ec23ae4b1b0032768a43972960096ef472dc0166
Ha Duc <haducvcvb@gmail.com>
2022-03-17 12:27:39 +0700

    header to nav adding button search

--- Main/index.php
+++ Main/index.php
@@ -4,9 +4,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Apple</title>
+    <link rel="stylesheet" href="./public/css/style.css">
+    <script src="https://kit.fontawesome.com/a70346401e.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Hello</h1>
+	<!-- ============ Begin: Main ============ -->
+    <div id="main">
+        <!-- Header -->
+        <div id="header">
+            <!-- Begin: Nav -->
+            <ul id="nav">
+                <li><a href="#"><i class="fa-brands fa-apple"></i></a></li>
+                <li><a href="#">Mac</a></li>
+                <li><a href="#">iPad</a></li>
+                <li><a href="#">iPhone</a></li>
+                <li><a href="#">Watch</a></li>
+                <li><a href="#">AirPods</a></li>
+                <li><a href="#">TV & Home</a></li>
+                <li><a href="#">Only on Apple</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Where to Buy</a></li>
+            </ul>
+            <!-- End: Nav -->
+
+            <div class="search-btn">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Slider -->
+        <div id="slider">
+
+        </div>
+        <!-- Content -->
+        <div id="content">
+            <div class="collections-content">
+                
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <div id="footer">
+            
+        </div>
+
+    </div>
+	<!-- ============ End: Main ============ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
