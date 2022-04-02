ddb20a32aa44cf2eadde447ea5dd089c7b4caf15
Ha Duc <haducvcvb@gmail.com>
2022-04-02 23:27:53 +0700

    commit save folder apple-clone before change to model mvc

---
+++ website/Main/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apple</title>
+    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="./public/fonts/themify-icons/themify-icons.css">
+    <link rel="icon" href="./public/icons/fav-icon.png" type="image/x-icon" />
+    <script src="https://kit.fontawesome.com/a70346401e.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+	<!-- ============ Begin: Main ============ -->
+    <div id="main">
+        <!-- Header -->
+        <div id="header">
+            <ul id="nav">
+                <li><a href="#"><i class="fa-brands fa-apple"></i></a></li>
+                <li><a href="#">Mac</a></li>
+                <li><a href="#">iPad</a></li>
+                <li><a href="#">iPhone</a></li>
+                <li><a href="#">Watch</a></li>
+                <li><a href="#">AirPods</a></li>
+                <li><a href="#">TV & Home</a></li>
+                <li><a href="#">Only on Apple</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Where to Buy</a></li>
+                <li><a href="#"><i class="ti-search search-btn"></i></a></li>
+            </ul>
+
+        </div>
+
+
+        <!-- Slider -->
+        <div id="slider">
+            <img src="./public/img/slider/ip_13.jpg" alt="">
+
+            <!-- <div class="text-content">
+                <a href="#">
+                    <h2 class="text-heading">iPhone 13 Pro</h2>
+                    <h2 class="sub-text">Oh. So. Pro.</h2>
+                </a>
+                <div class="buy-learn-btn">
+                    <a href="#">Learn More</a> <i class="ti-angle-right"></i>
+                    <a href="#">Buy</a> <i class="ti-angle-right"></i>
+                </div>
+
+                
+			</div> -->
+            
+
+
+
+                <!-- <a href="#"> <img src="./public/img/slider/ip_13.jpg" alt=""></a> -->
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Content -->
+        <div id="content">
+            <div class="collections-content">
+                
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <div id="footer">
+            
+        </div>
+
+    </div>
+	<!-- ============ End: Main ============ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
